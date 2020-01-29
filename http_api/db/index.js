6d2456590aa6736e067327a67c9e079d89c5efde
ysmdmg <44485074+lisknonanika@users.noreply.github.com>
2020-01-29 14:05:55 +0900

    Update index.js

--- http_api/db/index.js
+++ http_api/db/index.js
@@ -20,7 +20,7 @@ module.exports.findAnswerSenderIdByTargetId = async (targetId) => {
             SELECT trs."senderId"
               FROM trs
              WHERE trs."type" = 52
-               AND trs."asset" ->> 'data' = $1
+               AND trs."asset" ->> "data" = $1
         `;
         const result = await client.query(query, [targetId]);
         return {success: true, data: result.rows}
@@ -34,39 +34,58 @@ module.exports.findAnswerSenderIdByTargetId = async (targetId) => {
     }
 }
 
-module.exports.findQuestion = async (isOpen, senderId, offset) => {
+module.exports.findQuestion = async (isOpen, params) => {
     let client = undefined;
     try {
+        // set senderId (default 0L)
+        const senderId = params.senderId? params.senderId: '0L';
+        
+        // set offset (default 0)
+        const offset = params.offset? params.offset: 0;
+        
+        // set sortKey (timestamp or num or reward. default timestamp)
+        let sortKey = 'timestamp';
+        if (!params.sortKey || params.sortKey.toUpperCase == "TIMESTAMP") {
+            sortKey = 'timestamp';
+        } else if (params.sortKey.toUpperCase == "NUM") {
+            sortKey = 'trs51."asset" -> "quiz" ->> "num"';
+        } else if (params.sortKey.toUpperCase == "REWARD") {
+            sortKey = 'trs51."asset" -> "quiz" ->> "reward"';
+        }
+        
+        // set sortType (0: DESC, 1: ASC)
+        const sortType = params.sortType? "DESC": "ASC";
+        
         client = await pool.connect();
         
         const query = `
             SELECT trs51."id",
                    trs51."senderId",
                    trs51."timestamp",
-                   (trs51."asset" -> 'quiz' ->> 'question') as question,
-                   (trs51."asset" -> 'quiz' ->> 'reward')::INT as reward,
-                   (trs51."asset" -> 'quiz' ->> 'num')::INT as num,
+                   (trs51."asset" -> "quiz" ->> "question") as question,
+                   (trs51."asset" -> "quiz" ->> "reward")::INT as reward,
+                   (trs51."asset" -> "quiz" ->> "num")::INT as num,
                    (SELECT count(*)
                       FROM trs as trs52
                      WHERE trs52."type" = 52
-                       AND trs52."asset" ->> 'data' = trs51."id")::INT as answered
+                       AND trs52."asset" ->> "data" = trs51."id")::INT as answered
               FROM trs as trs51
              WHERE trs51."type" = 51
                AND (
-                       (trs51."asset" -> 'quiz' ->> 'num')::INT ${isOpen? '>': '<='} (
+                       (trs51."asset" -> "quiz" ->> "num")::INT ${isOpen? ">": "<="} (
                            SELECT count(*)
                             FROM trs as trs52
                            WHERE trs52."type" = 52
-                             AND trs52."asset" ->> 'data' = trs51."id"
+                             AND trs52."asset" ->> "data" = trs51."id"
                        )
-                       ${isOpen? 'AND NOT EXISTS': 'OR EXISTS'} (
+                       ${isOpen? "AND NOT EXISTS": "OR EXISTS"} (
                            SELECT 1 FROM trs WHERE trs."type" = 52 AND trs."senderId" = $1
                        )
                    )
-            ORDER BY timestamp DESC
-            LIMIT 100 OFFSET $2
+            ORDER BY ${sortKey} ${sortType}
+            LIMIT 100 OFFSET ${offset}
         `;
-        const result = await client.query(query, [senderId, offset]);
+        const result = await client.query(query, [senderId]);
         return {success: true, data: result.rows}
 
     } catch(err) {
