ad888a2d28dea475b57eae81b4146338ff3675a0
ysmdmg <44485074+lisknonanika@users.noreply.github.com>
2020-01-29 14:57:58 +0900

    Update index.js

--- http_api/index.js
+++ http_api/index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 const helmet = require('helmet');
 const { api_config } = require('../config');
-const { request, verify } = require('../utility');
+const { request, checkUtil } = require('../utility');
 
+const db = require('./db')
 const question = require('./transaction/question');
 const answer = require('./transaction/answer');
 
 const app = express();
-app.set('secret', api_config.secret);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(helmet());
@@ -18,26 +17,9 @@ const router = express.Router();
 app.use('/api', router);
 
 /**
- * auth
- */
-router.post('/auth', (req, res) => {
-    (async () => {
-        if (!req.body.address) {
-            res.json({success: false, messages: ["Address is required"]});
-            return;
-        }
-        const token = jwt.sign({address: req.body.address}, app.get('secret'));
-        res.json({result: true, token: token});
-
-    })().catch((err) => {
-        res.json({success: false, err: err});
-    });
-});
-
-/**
  * Question Transaction
  */
-router.post('/question', verify, (req, res) => {
+router.post('/question', (req, res) => {
     (async () => {
         // Validation
         const errors = await question.validator(req);
@@ -68,7 +50,7 @@ router.post('/question', verify, (req, res) => {
 /**
  * Answer Transaction
  */
-router.post('/answer', verify, (req, res) => {
+router.post('/answer', (req, res) => {
     (async () => {
         // Validation
         const errors = await answer.validator(req);
@@ -90,6 +72,67 @@ router.post('/answer', verify, (req, res) => {
         });
 
         res.json({success: true, response: data});
+
+    })().catch((err) => {
+        res.json({success: false, err: err});
+    });
+});
+
+/**
+ * Get Answers
+ * query: qid(required)
+ */
+router.get('/answers', (req, res) => {
+    (async () => {
+        // Validation
+        if (!req.query.qid) {
+            res.json({success: false, messages: 'Parameter "qid" is required.'});
+            return;
+        }
+
+        // GET
+        const answerTransactions = await db.findAnswerByQuestionId(req.query.qid)
+        if (!answerTransactions.success) {
+            res.json({success: false, messages: 'Failed to get answer data'});
+            return;
+        }
+
+        res.json({success: true, response: answerTransactions.data});
+
+    })().catch((err) => {
+        res.json({success: false, err: err});
+    });
+});
+
+/**
+ * Get Questions
+ * query: open, userId, offset, sortKey, sortType
+ */
+router.get('/questions', (req, res) => {
+    (async () => {
+        let isOpen = true;
+        let params = {
+            senderId: '0L',
+            offset: 0,
+            sortKey: 'timestamp',
+            sortType: 0,
+        }
+        
+        // Set Params
+        if (req.query.open && req.query.open == "0") isOpen = false;
+        if (req.query.userId) params.senderId = req.query.userId;
+        if (req.query.offset && checkUtil.checkNumber(req.query.offset)) params.offset = +req.query.offset;
+        if (req.query.sortKey) params.sortKey = req.query.sortKey;
+        if (req.query.sortType && req.query.sortType != "0") params.sortType = 1;
+        
+        // GET
+        const questionTransactions = await db.findQuestion(isOpen, params)
+        if (!questionTransactions.success) {
+            res.json({success: false, messages: 'Failed to get question data'});
+            return;
+        }
+
+        res.json({success: true, response: questionTransactions.data});
 
     })().catch((err) => {
         res.json({success: false, err: err});
